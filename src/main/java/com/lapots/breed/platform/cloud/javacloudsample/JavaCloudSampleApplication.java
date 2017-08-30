0a136698e12e9d7cfa89bb4fcfd5aa91760f6691
Lapots <sebastrident@gmail.com>
2017-08-30 20:40:59 +0300

    Spring Boot 1.5.6 support added

---
+++ src/main/java/com/lapots/breed/platform/cloud/javacloudsample/JavaCloudSampleApplication.java
@@ -0,0 +1,12 @@
+package com.lapots.breed.platform.cloud.javacloudsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCloudSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaCloudSampleApplication.class, args);
+	}
+}
